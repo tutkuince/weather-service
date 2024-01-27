7fa61dfc73ec168a5cc656dc567f942cbe8c54bd
Tutku Ince <tutku.ince@outlook.com>
2024-01-27 15:29:45 +0300

    Missing fields has been added to ResponseModel

--- src/main/java/io/tutkuince/weatherservice/dto/Current.java
+++ src/main/java/io/tutkuince/weatherservice/dto/Current.java
@@ -5,6 +5,33 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record Current(
-        Integer temperature
+        @JsonProperty("observation_time")
+        String observationTime,
+        Integer temperature,
+        @JsonProperty("weather_code")
+        Integer weather_code,
+        @JsonProperty("weather_icons")
+        List<String> weatherIcons,
+        @JsonProperty("weather_descriptions")
+        List<String> weatherDescriptions,
+        @JsonProperty("wind_speed")
+        Integer windSpeed,
+        @JsonProperty("wind_degree")
+        Integer windDegree,
+        @JsonProperty("wind_dir")
+        String windDir,
+        Integer pressure,
+        @JsonProperty("precip")
+        Integer preCip,
+        Integer humidity,
+        @JsonProperty("cloudcover")
+        Integer cloudCover,
+        @JsonProperty("feelslike")
+        Integer feelsLike,
+        @JsonProperty("uv_index")
+        Integer uvIndex,
+        Integer visibility,
+        @JsonProperty("is_day")
+        String isDay
 ) {
 }
--- src/main/java/io/tutkuince/weatherservice/dto/Location.java
+++ src/main/java/io/tutkuince/weatherservice/dto/Location.java
@@ -5,7 +5,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record Location(
         String name,
         String country,
+        String region,
+        String lat,
+        String lon,
+        @JsonProperty("timezone_id")
+        String timezoneId,
         @JsonProperty("localtime")
-        String localTime
+        String localTime,
+        @JsonProperty("localtime_epoch")
+        String localTimeEpoch,
+        @JsonProperty("utc_offset")
+        String utcOffset
 ) {
 }
