029ac17c112072dc3a61d8b485bdd1d748a30ab5
Tutku Ince <tutku.ince@outlook.com>
2024-01-27 19:01:49 +0300

    Validation

--- src/main/java/io/tutkuince/weatherservice/controller/WeatherController.java
+++ src/main/java/io/tutkuince/weatherservice/controller/WeatherController.java
@@ -1,8 +1,11 @@
 package io.tutkuince.weatherservice.controller;
 
+import io.tutkuince.weatherservice.controller.validation.CityNameConstraint;
 import io.tutkuince.weatherservice.dto.WeatherDto;
 import io.tutkuince.weatherservice.service.WeatherService;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/api/weather")
+@Validated
 public class WeatherController {
 
     private final WeatherService weatherService;
@@ -19,7 +23,7 @@ public class WeatherController {
     }
 
     @GetMapping("/{cityName}")
-    public ResponseEntity<WeatherDto> getWeatherByCityName(@PathVariable String cityName) {
+    public ResponseEntity<WeatherDto> getWeatherByCityName(@PathVariable @NotBlank @CityNameConstraint String cityName) {
         return ResponseEntity.ok(weatherService.getWeatherByCityName(cityName));
     }
 }
