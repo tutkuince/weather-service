8e1bafa5d65060daa3380e0d7c50456184e0b917
Tutku Ince <tutku.ince@outlook.com>
2024-01-27 16:35:25 +0300

    updated for UUID

--- src/main/java/io/tutkuince/weatherservice/dto/WeatherDto.java
+++ src/main/java/io/tutkuince/weatherservice/dto/WeatherDto.java
@@ -1,13 +1,22 @@
 package io.tutkuince.weatherservice.dto;
 
 import io.tutkuince.weatherservice.model.Weather;
+import io.tutkuince.weatherservice.util.DateUtil;
+
+import java.time.LocalDateTime;
 
 public record WeatherDto(
         String cityName,
         String country,
-        Integer temperature
+        Integer temperature,
+        String updatedTime
 ) {
     public static WeatherDto convertToWeatherDto(Weather from) {
-        return new WeatherDto(from.getCityName(), from.getCountry(), from.getTemperature());
+        return new WeatherDto(
+                from.getCityName(),
+                from.getCountry(),
+                from.getTemperature(),
+                from.getUpdatedTime().format(DateUtil.formatDateTime())
+        );
     }
 }
