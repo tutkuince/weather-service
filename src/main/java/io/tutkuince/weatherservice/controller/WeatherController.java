2caf723bcb060053467eaf140c50a8c155dcebd3
Tutku Ince <tutku.ince@outlook.com>
2024-01-28 21:46:28 +0300

    swagger config

--- src/main/java/io/tutkuince/weatherservice/controller/WeatherController.java
+++ src/main/java/io/tutkuince/weatherservice/controller/WeatherController.java
@@ -1,10 +1,17 @@
 package io.tutkuince.weatherservice.controller;
 
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import io.tutkuince.weatherservice.controller.validation.CityNameConstraint;
 import io.tutkuince.weatherservice.dto.WeatherDto;
 import io.tutkuince.weatherservice.service.WeatherService;
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/api/weather")
 @Validated
+@Tag(name = "Open Weather Service API v1", description = "Open Weather Service API to search the current weather report of the city")
 public class WeatherController {
 
     private final WeatherService weatherService;
@@ -23,6 +31,36 @@ public class WeatherController {
         this.weatherService = weatherService;
     }
 
+    @Operation(
+            method = "GET",
+            summary = "search the current weather report of the city",
+            description = "search the current weather report of the city name filter. The api has the rate limiting. 10 request per minute",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "The current weather report of the city",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = WeatherDto.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "City name is wrong. Re-try with a valid city name",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+                    @ApiResponse(
+                            responseCode = "429",
+                            description = "Rate limit exceeded. Please try your request again later!",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal/External server error.",
+                            content = @Content(schema = @Schema(hidden = true))
+                    )
+            }
+    )
     @GetMapping("/{cityName}")
     @RateLimiter(name = "basic")
     public ResponseEntity<WeatherDto> getWeatherByCityName(@PathVariable @NotBlank @CityNameConstraint String cityName) {
